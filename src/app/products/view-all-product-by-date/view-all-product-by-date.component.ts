95c25aa824ea63883afb7766b7f48be091edc6bd
premmakumar <premmakumar@outlook.com>
2021-01-12 09:37:15 +0530

    Modules and Components stubs added for orders and products

---
+++ src/app/products/view-all-product-by-date/view-all-product-by-date.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-view-all-product-by-date',
+  templateUrl: './view-all-product-by-date.component.html',
+  styleUrls: ['./view-all-product-by-date.component.css']
+})
+export class ViewAllProductByDateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
